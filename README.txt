8ebf21826a72280e3a1e7d9d69e49ea8c26a6584
Onkar Thite <omkartht@gmail.com>
2026-05-26 06:56:03 +0530

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,16 @@
+
+Movie Recommendation System Portfolio Website
+
+Files Included:
+- index.html
+- about.html
+- projects.html
+- phases.html
+- phase4.html
+- contact.html
+- style.css
+- script.js
+- banner.png
+
+How to Run:
+Open index.html in your browser.
---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    const btn=document.getElementById("welcomeBtn");
+    if(btn){
+        btn.addEventListener("click", ()=>{
+            alert("Welcome to Onkar Thite's Portfolio Website!");
+        });
+    }
+});
